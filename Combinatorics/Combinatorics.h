9d43da8c1a379ec7ab67a7e793f571808c3434a6
Albert Mata <hello@albertmata.net>
2016-07-29 17:55:39 +0200

    Initial Commit

---
+++ Combinatorics/Combinatorics.h
@@ -0,0 +1,19 @@
+//
+//  Combinatorics.h
+//  Combinatorics
+//
+//  Created by Albert Mata Guerra on 29/07/16.
+//  Copyright © 2016 Albert Mata Guerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Combinatorics.
+FOUNDATION_EXPORT double CombinatoricsVersionNumber;
+
+//! Project version string for Combinatorics.
+FOUNDATION_EXPORT const unsigned char CombinatoricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Combinatorics/PublicHeader.h>
+
+
